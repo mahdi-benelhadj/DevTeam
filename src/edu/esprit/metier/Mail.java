ea7a788947b649e1cc12663460ae7787209c018b
GHADA <GHADA@GHADA-PC>
2014-03-05 17:44:01 +0100

    supprimer client

---
+++ src/edu/esprit/metier/Mail.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.esprit.metier;
+
+/**
+ *
+ * @author GHADA
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author MohamedAmine
+ */
+public class Mail {
+    
+    
+   
+    private String mailAddressSender="bestdealpi@gmail.com" ;
+    private String pwd ="bestdeal5";
+    private String mailAddressRecipient ; 
+    private String mailObject="VOUCHER" ;
+    
+
+    public String getMailAddressSender() {
+        return mailAddressSender;
+    }
+
+    public void setMailAddressSender(String mailAddressSender) {
+        this.mailAddressSender = mailAddressSender;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getMailAddressRecipient() {
+        return mailAddressRecipient;
+    }
+
+    public void setMailAddressRecipient(String mailAddressRecipient) {
+        this.mailAddressRecipient = mailAddressRecipient;
+    }
+
+    public String getMailObject() {
+        return mailObject;
+    }
+
+    public void setMailObject(String mailObject) {
+        this.mailObject = mailObject;
+    }
+
+   
+    }
+   
+  
+    
+
+    
+    
+
